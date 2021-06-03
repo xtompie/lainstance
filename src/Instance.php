73f15de9e86df81107a36c5ecff2e73a697452d6
Tomasz Pielczyk <tomasz.pielczyk@gmail.com>
2021-06-03 22:38:19 +0200

    return type for php8

--- src/Instance.php
+++ src/Instance.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\App;
 
 trait Instance
 {
-    public static function instance()
+    public static function instance(): static
     {
         return App::make(static::class);
     }
